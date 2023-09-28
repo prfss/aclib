a2004506e505d36cad11f862736df2248eb8ba9e
prfss <gprfss4815@gmail.com>
2023-09-29 00:42:58 +0900

    いくつかのモジュールの削除

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,9 @@
 #![allow(clippy::many_single_char_names)]
-pub mod at;
 pub mod boolarray;
 pub mod chooser;
 pub mod cmp;
 pub mod fastarray;
-pub mod fenwicktree;
-pub mod io;
-pub mod lazysegtree;
-pub mod maxflow;
-pub mod mincostflow;
-pub mod monoid;
-pub mod number;
 pub mod point;
 pub mod quickselect;
-pub mod scc;
-pub mod segtree;
 pub mod simplechooser;
 pub mod timer;
-pub mod unionfind;
