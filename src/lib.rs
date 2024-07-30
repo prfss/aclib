9eae69580d8a32c19da207a789289dfcf3ae8a24
prfss <gprfss4815@gmail.com>
2024-07-30 20:16:34 +0900

    `lib.rs`の更新

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 #![allow(clippy::many_single_char_names)]
 pub mod boolarray;
 pub mod chooser;
-pub mod cmp;
 pub mod fastarray;
+pub mod kdtree;
+pub mod lca;
 pub mod point;
 pub mod simplechooser;
-pub mod timer;
