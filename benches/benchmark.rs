cc0024aa2e6d3adae2385e9fc80b413e7e9a9e59
prfss <gprfss4815@gmail.com>
2024-08-17 13:32:19 +0900

    benchmark.rsの更新

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -4,69 +4,48 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use rand::{prelude::*, SeedableRng};
 use rand_pcg::Pcg64;
 
+fn bench_kd_tree_creation_internal(c: &mut Criterion, n: usize, dim: usize) {
+    c.bench_function(
+        &format!("kdtree creation (n={}, dim={})", n, dim),
+        move |b| {
+            let mut rng = Pcg64::seed_from_u64(3141592653);
+            let vs: Vec<_> = (0..n)
+                .map(|_| (0..dim).map(|_| rng.gen_range(-10000.0..10000.0)).collect())
+                .collect();
+            b.iter(|| kdtree::KdTree::from_vec(&mut vs.clone()));
+        },
+    );
+}
+
 fn bench_kdtree_creation(c: &mut Criterion) {
-    c.bench_function("kdtree creation (c=100, d=2)", |b| {
-        let mut v = Vec::new();
-        for _ in 0..100 {
-            v.push((0..2).map(|_| rand::random::<f64>()).collect());
-        }
-        b.iter(|| kdtree::KdTree::from_vec(&mut v.clone()));
-    });
+    bench_kd_tree_creation_internal(c, 100, 2);
+    bench_kd_tree_creation_internal(c, 100, 3);
+    bench_kd_tree_creation_internal(c, 1000, 2);
+    bench_kd_tree_creation_internal(c, 1000, 3);
+}
 
-    c.bench_function("kdtree creation (c=100, d=3)", |b| {
-        let mut v = Vec::new();
-        for _ in 0..100 {
-            v.push((0..3).map(|_| rand::random::<f64>()).collect());
-        }
-        b.iter(|| kdtree::KdTree::from_vec(&mut v.clone()));
-    });
+fn bench_kdtree_find_internal(c: &mut Criterion, n: usize, dim: usize) {
+    c.bench_function(&format!("kdtree find (n={}, dim={})", n, dim), move |b| {
+        let mut rng = Pcg64::seed_from_u64(3141592653);
+        let mut vs: Vec<_> = (0..n)
+            .map(|_| (0..dim).map(|_| rng.gen_range(-10000.0..10000.0)).collect())
+            .collect();
 
-    c.bench_function("kdtree creation (c=1000, d=2)", |b| {
-        let mut v = Vec::new();
-        for _ in 0..1000 {
-            v.push((0..2).map(|_| rand::random::<f64>()).collect());
-        }
-        b.iter(|| kdtree::KdTree::from_vec(&mut v.clone()));
-    });
-
-    c.bench_function("kdtree creation (c=1000, d=3)", |b| {
-        let mut v = Vec::new();
-        for _ in 0..1000 {
-            v.push((0..3).map(|_| rand::random::<f64>()).collect());
-        }
-        b.iter(|| kdtree::KdTree::from_vec(&mut v.clone()));
-    });
-
-    c.bench_function("kdtree creation (c=10000, d=2)", |b| {
-        let mut v = Vec::new();
-        for _ in 0..10000 {
-            v.push((0..2).map(|_| rand::random::<f64>()).collect());
-        }
-        b.iter(|| kdtree::KdTree::from_vec(&mut v.clone()));
-    });
-
-    c.bench_function("kdtree creation (c=10000, d=3)", |b| {
-        let mut v = Vec::new();
-        for _ in 0..10000 {
-            v.push((0..3).map(|_| rand::random::<f64>()).collect());
-        }
-        b.iter(|| kdtree::KdTree::from_vec(&mut v.clone()));
+        let tree = kdtree::KdTree::from_vec(&mut vs);
+        b.iter(|| {
+            let query: Vec<_> = (0..dim).map(|_| rng.gen_range(-10000.0..10000.0)).collect();
+            tree.find_nearest_neighbor(&query);
+        });
     });
 }
 
 fn bench_kdtree_find(c: &mut Criterion) {
-    let mut rng = Pcg64::seed_from_u64(3141592653);
-    c.bench_function("kdtree find (c=10000, d=3)", move |b| {
-        let mut v = Vec::new();
-        for _ in 0..10000 {
-            v.push((0..3).map(|_| rng.gen_range(-10.0..10.0)).collect());
-        }
-        let tree = kdtree::KdTree::from_vec(&mut v);
-        b.iter(|| {
-            let query: Vec<_> = (0..3).map(|_| rng.gen_range(-10.0..10.0)).collect();
-            tree.find_nearest_neighbor(&query);
-        });
-    });
+    bench_kdtree_find_internal(c, 100, 2);
+    bench_kdtree_find_internal(c, 100, 3);
+    bench_kdtree_find_internal(c, 1000, 2);
+    bench_kdtree_find_internal(c, 1000, 3);
+    bench_kdtree_find_internal(c, 10000, 2);
+    bench_kdtree_find_internal(c, 10000, 3);
 }
 
 criterion_group!(benches, bench_kdtree_creation, bench_kdtree_find);
