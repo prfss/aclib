53f2cfdde603b87d3d82c7b96cd035aebf96ee32
prfss <gprfss4815@gmail.com>
2024-08-18 12:32:58 +0900

    Squashed commit of the following:
    commit af30e7bb50990adb4d15f39fd9fdff906dde160b
    Author: prfss <gprfss4815@gmail.com>
    Date:   Sun Aug 18 12:18:39 2024 +0900
    
        範囲クエリのベンチマークの追加
    
    commit f2b73ffe9fdbb13d6fc646d2abbd1222dda78fd2
    Author: prfss <gprfss4815@gmail.com>
    Date:   Sun Aug 18 12:18:16 2024 +0900
    
        トレイトの仕様変更
    
        - トレイトに(デフォルト実装のある)メソッドを追加した
        - `KdPoint`,`Rect`でそれらのメソッドを実装した
        - それらのメソッドを内部で呼び出すようにした
    
    commit 519549f07ddec15b1f53f4d856bc8671a3351388
    Author: prfss <gprfss4815@gmail.com>
    Date:   Sun Aug 18 11:49:28 2024 +0900
    
        クエリトレイトを`KdNode`ではなく`Rect`二関して抽象化
    
    commit befc6b921f7b2638b4fbad2858c73e75ba9a24a2
    Author: prfss <gprfss4815@gmail.com>
    Date:   Sun Aug 18 10:50:11 2024 +0900
    
        `KdTree`の機能追加
    
        - 最近傍クエリの抽象化
        - 範囲クエリ
    
    commit da1ac6ecd35765d6d3380659722c0a4df8416b22
    Author: prfss <gprfss4815@gmail.com>
    Date:   Sat Aug 17 17:27:59 2024 +0900
    
        リファクタリング

--- benches/kdtree.rs
+++ benches/kdtree.rs
@@ -24,7 +24,7 @@ fn bench_kdtree_creation(c: &mut Criterion) {
     bench_kd_tree_creation_internal(c, 1000, 3);
 }
 
-fn bench_kdtree_find_internal(c: &mut Criterion, n: usize, dim: usize) {
+fn bench_kdtree_fnn_internal(c: &mut Criterion, n: usize, dim: usize) {
     c.bench_function(&format!("kdtree find (n={}, dim={})", n, dim), move |b| {
         let mut rng = Pcg64::seed_from_u64(3141592653);
         let mut vs: Vec<_> = (0..n)
@@ -39,14 +39,47 @@ fn bench_kdtree_find_internal(c: &mut Criterion, n: usize, dim: usize) {
     });
 }
 
-fn bench_kdtree_find(c: &mut Criterion) {
-    bench_kdtree_find_internal(c, 100, 2);
-    bench_kdtree_find_internal(c, 100, 3);
-    bench_kdtree_find_internal(c, 1000, 2);
-    bench_kdtree_find_internal(c, 1000, 3);
-    bench_kdtree_find_internal(c, 10000, 2);
-    bench_kdtree_find_internal(c, 10000, 3);
+fn bench_kdtree_fnn(c: &mut Criterion) {
+    bench_kdtree_fnn_internal(c, 100, 2);
+    bench_kdtree_fnn_internal(c, 100, 3);
+    bench_kdtree_fnn_internal(c, 1000, 2);
+    bench_kdtree_fnn_internal(c, 1000, 3);
+    bench_kdtree_fnn_internal(c, 10000, 2);
+    bench_kdtree_fnn_internal(c, 10000, 3);
 }
 
-criterion_group!(benches, bench_kdtree_creation, bench_kdtree_find);
+fn bench_kdtree_range_internal(c: &mut Criterion, n: usize, dim: usize) {
+    c.bench_function(&format!("kdtree range (n={}, dim={})", n, dim), move |b| {
+        let mut rng = Pcg64::seed_from_u64(3141592653);
+        let mut vs: Vec<_> = (0..n)
+            .map(|_| (0..dim).map(|_| rng.gen_range(-10000.0..10000.0)).collect())
+            .collect();
+        let tree = kdtree::KdTree::from_vec(&mut vs);
+        b.iter(|| {
+            let dx = rng.gen_range(1000.0..5000.0);
+            let dy = rng.gen_range(1000.0..5000.0);
+            let x = rng.gen_range(-10000.0..10000.0 - dx);
+            let y = rng.gen_range(-10000.0..10000.0 - dy);
+            let query = kdtree::Rect::new(vec![x, y], vec![x + dx, y + dy]);
+
+            tree.find_in_range(&query);
+        });
+    });
+}
+
+fn bench_kdtree_range(c: &mut Criterion) {
+    bench_kdtree_range_internal(c, 100, 2);
+    bench_kdtree_range_internal(c, 100, 3);
+    bench_kdtree_range_internal(c, 1000, 2);
+    bench_kdtree_range_internal(c, 1000, 3);
+    bench_kdtree_range_internal(c, 10000, 2);
+    bench_kdtree_range_internal(c, 10000, 3);
+}
+
+criterion_group!(
+    benches,
+    bench_kdtree_creation,
+    bench_kdtree_fnn,
+    bench_kdtree_range
+);
 criterion_main!(benches);
--- src/kdtree.rs
+++ src/kdtree.rs
@@ -1,30 +1,34 @@
-//! ユークリッド空間における最近傍探索を行います.
+//! ユークリッド空間における点の索引
 use ordered_float::OrderedFloat;
 
-/// $n$次元ユークリッド空間の最近傍点を求める構造体です
+/// $n$次元ユークリッド空間における点の索引を表す構造体です
 #[derive(Clone, Debug)]
 pub struct KdTree {
     nodes: Vec<KdNode>,
 }
 
+pub type KdPoint = Vec<f64>;
+
 impl KdTree {
-    /// `vs`からなる点の集合を作ります.
-    pub fn from_vec(vs: &mut [Vec<f64>]) -> Self {
+    /// `vs`からなる索引を作ります.
+    pub fn from_vec(vs: &mut [KdPoint]) -> Self {
         let mut nodes = Vec::with_capacity(2 * vs.len());
         Self::from_vec_internal(vs, 0, &mut nodes);
         Self { nodes }
     }
 
-    /// この集合に含まれる点のうち,$p$に最も近い点を返します.
-    pub fn find_nearest_neighbor(&self, p: &[f64]) -> Option<(f64, &Vec<f64>)> {
-        assert!(!p.is_empty());
+    /// この索引に含まれる点のうち,`query`に最も近い点を返します.
+    pub fn find_nearest_neighbor<Q>(&self, query: &Q) -> Option<(f64, &KdPoint)>
+    where
+        Q: KdFnnQuery,
+    {
         let mut dist2 = std::f64::MAX;
         let mut np = None;
-        self.find_nearest_neighbor_internal(self.nodes.len() - 1, p, &mut dist2, &mut np);
+        self.find_nearest_neighbor_internal(self.nodes.len() - 1, query, &mut dist2, &mut np);
         np.map(|np| (dist2.sqrt(), np))
     }
 
-    fn from_vec_internal(vs: &mut [Vec<f64>], axis: usize, nodes: &mut Vec<KdNode>) -> usize {
+    fn from_vec_internal(vs: &mut [KdPoint], axis: usize, nodes: &mut Vec<KdNode>) -> usize {
         assert!(!vs.is_empty());
 
         let dim = vs[0].len();
@@ -42,89 +46,134 @@ impl KdTree {
             let left = KdTree::from_vec_internal(pre, new_axis, nodes);
             let right = KdTree::from_vec_internal(suf, new_axis, nodes);
             let bottom_left = (0..dim)
-                .map(|axis| {
-                    nodes[left]
-                        .bottom_left(axis)
-                        .min(nodes[right].bottom_left(axis))
-                })
+                .map(|axis| nodes[left].l(axis).min(nodes[right].l(axis)))
                 .collect();
             let top_right = (0..dim)
-                .map(|axis| {
-                    nodes[left]
-                        .top_right(axis)
-                        .max(nodes[right].top_right(axis))
-                })
+                .map(|axis| nodes[left].u(axis).max(nodes[right].u(axis)))
                 .collect();
 
-            let rect = Rect::new(bottom_left, top_right);
-            nodes.push(KdNode::Internal { rect, left, right });
+            let mbb = Rect::new(bottom_left, top_right);
+            nodes.push(KdNode::Internal { mbb, left, right });
         }
         nodes.len() - 1
     }
 
-    fn find_nearest_neighbor_internal<'a>(
+    fn find_nearest_neighbor_internal<'a, Q>(
         &'a self,
         node_i: usize,
-        p: &[f64],
-        min_dist2: &mut f64,
-        np: &mut Option<&'a Vec<f64>>,
-    ) {
-        if self.nodes[node_i].euclidean_distance2(p) > *min_dist2 {
+        query: &Q,
+        min_dist: &mut f64,
+        np: &mut Option<&'a KdPoint>,
+    ) where
+        Q: KdFnnQuery,
+    {
+        let dist = Self::node_dist(query, &self.nodes[node_i]);
+        if dist >= *min_dist {
             return;
         }
 
-        let (&left, &right) = match &self.nodes[node_i] {
+        match &self.nodes[node_i] {
             KdNode::Leaf { ref point } => {
-                let dist2 = (0..p.len())
-                    .map(|i| (point[i] - p[i]) * (point[i] - p[i]))
-                    .sum::<f64>();
-                if dist2 < *min_dist2 {
-                    *min_dist2 = dist2;
+                if dist < *min_dist {
+                    *min_dist = dist;
                     *np = Some(point);
                 }
                 return;
             }
-            KdNode::Internal { left, right, .. } => (left, right),
+            KdNode::Internal { left, right, .. } => {
+                let left = *left;
+                let right = *right;
+                let left_dist = Self::node_dist(query, &self.nodes[left]);
+                let right_dist = Self::node_dist(query, &self.nodes[right]);
+
+                if left_dist < right_dist {
+                    self.find_nearest_neighbor_internal(left, query, min_dist, np);
+                    self.find_nearest_neighbor_internal(right, query, min_dist, np);
+                } else {
+                    self.find_nearest_neighbor_internal(right, query, min_dist, np);
+                    self.find_nearest_neighbor_internal(left, query, min_dist, np);
+                }
+            }
         };
+    }
 
-        let left_dist = self.nodes[left].euclidean_distance2(p);
-        let right_dist = self.nodes[right].euclidean_distance2(p);
-
-        if left_dist < right_dist {
-            self.find_nearest_neighbor_internal(left, p, min_dist2, np);
-            self.find_nearest_neighbor_internal(right, p, min_dist2, np);
-        } else {
-            self.find_nearest_neighbor_internal(right, p, min_dist2, np);
-            self.find_nearest_neighbor_internal(left, p, min_dist2, np);
+    fn node_dist<Q>(query: &Q, node: &KdNode) -> f64
+    where
+        Q: KdFnnQuery,
+    {
+        match node {
+            KdNode::Internal { mbb, .. } => query.mbb_dist(mbb),
+            KdNode::Leaf { point } => query.dist(point),
         }
+    }
+
+    /// この索引に含まれる点のうち,`query`に含まれる点をすべて返します.
+    pub fn find_in_range<Q>(&self, query: &Q) -> Vec<&KdPoint>
+    where
+        Q: KdRangeQuery,
+    {
+        let mut res = vec![];
+        self.find_in_range_internal(self.nodes.len() - 1, query, &mut res);
+        res
+    }
+
+    fn find_in_range_internal<'a, Q>(&'a self, node_i: usize, query: &Q, res: &mut Vec<&'a KdPoint>)
+    where
+        Q: KdRangeQuery,
+    {
+        match &self.nodes[node_i] {
+            KdNode::Leaf { ref point } => {
+                if query.includes(point) {
+                    res.push(point);
+                }
+            }
+            KdNode::Internal { mbb, left, right } => {
+                if !query.mbb_overlaps(mbb) {
+                    return;
+                }
+
+                self.find_in_range_internal(*left, query, res);
+                self.find_in_range_internal(*right, query, res);
+            }
+        };
     }
 }
 
 #[derive(Clone, Debug)]
-struct Rect {
-    bottom_left: Vec<f64>,
-    top_right: Vec<f64>,
+pub struct Rect {
+    range: Vec<(f64, f64)>,
 }
 
 impl Rect {
-    fn new(bottom_left: Vec<f64>, top_right: Vec<f64>) -> Self {
+    pub fn dim(&self) -> usize {
+        self.range.len()
+    }
+
+    pub fn new(bottom_left: Vec<f64>, top_right: Vec<f64>) -> Self {
         assert_eq!(bottom_left.len(), top_right.len());
         assert!((0..bottom_left.len()).all(|i| bottom_left[i] <= top_right[i]));
         Self {
-            bottom_left,
-            top_right,
+            range: bottom_left.into_iter().zip(top_right).collect(),
         }
     }
-    #[inline]
-    fn euclidean_distance2(&self, p: &[f64]) -> f64 {
-        (0..self.bottom_left.len())
-            .map(|i| {
-                if self.bottom_left[i] <= p[i] && p[i] <= self.top_right[i] {
+
+    pub fn l(&self, axis: usize) -> f64 {
+        self.range[axis].0
+    }
+
+    pub fn u(&self, axis: usize) -> f64 {
+        self.range[axis].1
+    }
+
+    pub fn euclidean_distance2(&self, p: &KdPoint) -> f64 {
+        (0..self.dim())
+            .map(|axis| {
+                if self.l(axis) <= p[axis] && p[axis] <= self.u(axis) {
                     0.0
                 } else {
-                    let d = (p[i] - self.bottom_left[i])
+                    let d = (p[axis] - self.l(axis))
                         .abs()
-                        .min((p[i] - self.top_right[i]).abs());
+                        .min((p[axis] - self.u(axis)).abs());
                     d * d
                 }
             })
@@ -132,43 +181,76 @@ impl Rect {
     }
 }
 
+impl From<&KdPoint> for Rect {
+    fn from(value: &KdPoint) -> Self {
+        Self {
+            range: value.iter().map(|x| (*x, *x)).collect(),
+        }
+    }
+}
+
 #[derive(Clone, Debug)]
 enum KdNode {
     Internal {
-        rect: Rect,
+        mbb: Rect,
         left: usize,
         right: usize,
     },
     Leaf {
-        point: Vec<f64>,
+        point: KdPoint,
     },
 }
 
 impl KdNode {
-    #[inline]
-    fn bottom_left(&self, axis: usize) -> f64 {
+    fn l(&self, axis: usize) -> f64 {
         match self {
-            KdNode::Internal { rect, .. } => rect.bottom_left[axis],
+            KdNode::Internal { mbb: rect, .. } => rect.l(axis),
             KdNode::Leaf { point } => point[axis],
         }
     }
 
-    #[inline]
-    fn top_right(&self, axis: usize) -> f64 {
+    fn u(&self, axis: usize) -> f64 {
         match self {
-            KdNode::Internal { rect, .. } => rect.top_right[axis],
+            KdNode::Internal { mbb: rect, .. } => rect.u(axis),
             KdNode::Leaf { point } => point[axis],
         }
     }
+}
 
-    #[inline]
-    fn euclidean_distance2(&self, p: &[f64]) -> f64 {
-        match self {
-            KdNode::Internal { rect, .. } => rect.euclidean_distance2(p),
-            KdNode::Leaf { point } => (0..point.len())
-                .map(|i| (point[i] - p[i]) * (point[i] - p[i]))
-                .sum::<f64>(),
-        }
+pub trait KdFnnQuery {
+    fn dist(&self, p: &KdPoint) -> f64 {
+        self.mbb_dist(&Rect::from(p))
+    }
+    fn mbb_dist(&self, mbb: &Rect) -> f64;
+}
+
+impl KdFnnQuery for Vec<f64> {
+    fn dist(&self, p: &KdPoint) -> f64 {
+        (0..self.len())
+            .map(|axis| (self[axis] - p[axis]) * (self[axis] - p[axis]))
+            .sum()
+    }
+
+    fn mbb_dist(&self, mbb: &Rect) -> f64 {
+        mbb.euclidean_distance2(self)
+    }
+}
+
+pub trait KdRangeQuery {
+    fn includes(&self, p: &KdPoint) -> bool {
+        self.mbb_overlaps(&Rect::from(p))
+    }
+    fn mbb_overlaps(&self, mbb: &Rect) -> bool;
+}
+
+impl KdRangeQuery for Rect {
+    fn includes(&self, p: &KdPoint) -> bool {
+        (0..self.dim()).all(|i| self.range[i].0 <= p[i] && p[i] <= self.range[i].1)
+    }
+
+    fn mbb_overlaps(&self, mbb: &Rect) -> bool {
+        (0..self.dim())
+            .all(|i| mbb.range[i].0.max(self.range[i].0) <= mbb.range[i].1.min(self.range[i].1))
     }
 }
 
@@ -178,28 +260,38 @@ mod tests {
     use rand::prelude::*;
     use rand_pcg::Pcg64;
 
+    fn gen_kdtree<R: Rng>(n: usize, dim: usize, rng: &mut R) -> (KdTree, Vec<KdPoint>) {
+        let mut vs: Vec<_> = (0..n)
+            .map(|_| (0..dim).map(|_| rng.gen_range(-10000.0..10000.0)).collect())
+            .collect();
+
+        let tree = KdTree::from_vec(&mut vs);
+
+        (tree, vs)
+    }
+
     #[test]
-    fn kdtree_works() {
-        let mut rng = Pcg64::seed_from_u64(3141592653);
-        let dim = 5;
+    fn kdtree_fnn_works() {
         let point_count = 10000;
         let query_count = 100;
-        let mut vs: Vec<_> = (0..point_count)
-            .map(|_| (0..dim).map(|_| rng.gen_range(-10.0..10.0)).collect())
+        let dim = 5;
+        let mut rng = Pcg64::seed_from_u64(3141592653);
+
+        let (tree, vs) = gen_kdtree(point_count, dim, &mut rng);
+
+        let qs: Vec<Vec<f64>> = (0..query_count)
+            .map(|_| (0..dim).map(|_| rng.gen_range(-10000.0..10000.0)).collect())
             .collect();
-        let ps: Vec<Vec<f64>> = (0..query_count)
-            .map(|_| (0..dim).map(|_| rng.gen_range(-10.0..10.0)).collect())
-            .collect();
-        let tree = KdTree::from_vec(&mut vs);
-        for p in ps {
-            let np1 = tree.find_nearest_neighbor(&p).unwrap().1;
+
+        for q in qs {
+            let np1 = tree.find_nearest_neighbor(&q).unwrap().1;
             // 全探索解
             let np2 = vs
                 .iter()
                 .min_by_key(|x| {
                     OrderedFloat(
                         x.iter()
-                            .zip(&p)
+                            .zip(&q)
                             .map(|(a, b)| (a - b).powf(2.0))
                             .sum::<f64>(),
                     )
@@ -212,16 +304,91 @@ mod tests {
     #[test]
     fn kdtree_find_self() {
         let mut rng = Pcg64::seed_from_u64(3141592653);
-        let dim = 5;
-        let point_count = 1000;
-        let mut vs: Vec<_> = (0..point_count)
-            .map(|_| (0..dim).map(|_| rng.gen_range(-10.0..10.0)).collect())
-            .collect();
+        let (tree, vs) = gen_kdtree(100, 5, &mut rng);
 
-        let tree = KdTree::from_vec(&mut vs);
         for v in vs {
             let r = tree.find_nearest_neighbor(&v).map(|r| r.1);
             assert_eq!(r, Some(&v));
+        }
+    }
+
+    #[test]
+    fn kdtree_rect_range_works() {
+        let mut rng = Pcg64::seed_from_u64(3141592653);
+        let n = 1000;
+        let dim = 2;
+        let (tree, vs) = gen_kdtree(n, dim, &mut rng);
+        let query_count = 100;
+        let qs: Vec<_> = (0..query_count)
+            .map(|_| {
+                let dx = rng.gen_range(1000.0..5000.0);
+                let dy = rng.gen_range(1000.0..5000.0);
+                let x = rng.gen_range(-10000.0..10000.0 - dx);
+                let y = rng.gen_range(-10000.0..10000.0 - dy);
+                Rect::new(vec![x, y], vec![x + dx, y + dy])
+            })
+            .collect();
+        for q in qs {
+            let mut naive: Vec<_> = vs
+                .iter()
+                .filter(|v| (0..q.dim()).all(|i| q.range[i].0 <= v[i] && v[i] <= q.range[i].1))
+                .collect();
+            naive.sort_by(|a, b| a.partial_cmp(b).unwrap());
+            let mut ans = tree.find_in_range(&q);
+            ans.sort_by(|a, b| a.partial_cmp(b).unwrap());
+
+            assert_eq!(naive, ans);
+        }
+    }
+
+    struct Sphere {
+        center: Vec<f64>,
+        radius2: f64,
+    }
+
+    impl Sphere {
+        fn new(center: Vec<f64>, radius2: f64) -> Self {
+            Self { center, radius2 }
+        }
+    }
+
+    impl KdRangeQuery for Sphere {
+        fn mbb_overlaps(&self, mbb: &Rect) -> bool {
+            mbb.euclidean_distance2(&self.center) <= self.radius2
+        }
+    }
+
+    #[test]
+    fn kdtree_circle_range_works() {
+        let mut rng = Pcg64::seed_from_u64(3141592653);
+        let n = 1000;
+        let dim = 2;
+        let (tree, vs) = gen_kdtree(n, dim, &mut rng);
+        let query_count = 100;
+        let qs: Vec<_> = (0..query_count)
+            .map(|_| {
+                let center = (0..dim).map(|_| rng.gen_range(-10000.0..10000.0)).collect();
+                let radius2 = rng.gen_range(1_000_000.0..25_000_000.0);
+                Sphere::new(center, radius2)
+            })
+            .collect();
+
+        for q in qs {
+            let mut naive: Vec<_> = vs
+                .iter()
+                .filter(|v| {
+                    (0..dim)
+                        .map(|i| v[i] - q.center[i])
+                        .map(|x| x * x)
+                        .sum::<f64>()
+                        <= q.radius2
+                })
+                .collect();
+            naive.sort_by(|a, b| a.partial_cmp(b).unwrap());
+            let mut ans = tree.find_in_range(&q);
+            ans.sort_by(|a, b| a.partial_cmp(b).unwrap());
+
+            assert_eq!(naive, ans);
         }
     }
 }
