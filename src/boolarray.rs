677e40c25992abd67af7cfc155743c7293684e3e
prfss <gprfss4815@gmail.com>
2024-09-03 21:21:07 +0900

    `count_ones`/`count_zeros`を追加

--- src/boolarray.rs
+++ src/boolarray.rs
@@ -1,9 +1,11 @@
 //! 固定長のブール配列です.
 //!
 //! 配列の初期化を$O(1)$で行うことができます.
+#[derive(Clone)]
 pub struct BoolArray {
     data: Vec<usize>,
     threshold: usize,
+    pop_count: usize,
 }
 
 impl BoolArray {
@@ -14,6 +16,7 @@ impl BoolArray {
         BoolArray {
             data: vec![0; n],
             threshold: 1,
+            pop_count: 0,
         }
     }
 
@@ -23,6 +26,9 @@ impl BoolArray {
     /// # 計算量
     /// - $O(1)$
     pub fn set(&mut self, i: usize, x: bool) {
+        self.pop_count = self
+            .pop_count
+            .wrapping_add((x as usize).wrapping_sub(self.data[i]));
         self.data[i] = if x { self.threshold } else { 0 };
     }
 
@@ -35,6 +41,20 @@ impl BoolArray {
         self.data[i] == self.threshold
     }
 
+    /// `true`である要素の数を取得します.
+    /// # 計算量
+    /// - $O(1)$
+    pub fn count_ones(&self) -> usize {
+        self.pop_count
+    }
+
+    /// `false`である要素の数を取得します.
+    /// # 計算量
+    /// - $O(1)$
+    pub fn count_zeros(&self) -> usize {
+        self.data.len() - self.count_ones()
+    }
+
     /// すべての要素を$\mathrm{false}$にします.
     /// # 制約
     /// - このメソッドを呼ぶ回数は[`std::usize::MAX`](https://doc.rust-lang.org/std/usize/constant.MAX.html)回未満
@@ -42,5 +62,36 @@ impl BoolArray {
     /// - $O(1)$
     pub fn clear(&mut self) {
         self.threshold += 1;
+        self.pop_count = 0;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn bool_array_count_ones() {
+        let n = 5;
+        let mut a = BoolArray::new(n);
+        a.set(0, true);
+        a.set(1, true);
+        a.set(2, false);
+        a.set(3, true);
+        a.set(4, false);
+        assert_eq!(a.count_ones(), 3);
+        assert_eq!(a.count_zeros(), 2);
+        a.set(0, true);
+        assert_eq!(a.count_ones(), 3);
+        assert_eq!(a.count_zeros(), 2);
+        a.set(0, false);
+        assert_eq!(a.count_ones(), 2);
+        assert_eq!(a.count_zeros(), 3);
+        a.set(2, true);
+        assert_eq!(a.count_ones(), 3);
+        assert_eq!(a.count_zeros(), 2);
+        a.clear();
+        assert_eq!(a.count_ones(), 0);
+        assert_eq!(a.count_zeros(), 5);
     }
 }
